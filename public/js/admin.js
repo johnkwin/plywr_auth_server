8f920ac2ff296a009f074ee6c3f011ed27c61fe2
John K <johnkwin@gmail.com>
2024-06-16 21:00:49 -0400

    adm

--- public/js/admin.js
+++ public/js/admin.js
@@ -158,6 +158,8 @@ document.addEventListener('DOMContentLoaded', function () {
             })
             .then(response => response.json())
             .then(data => {
+                console.log(response.json());
+                console.log(data);
                 if (data.success) {
                     button.style.display = 'none';
                     console.log('User updated successfully:', data);
