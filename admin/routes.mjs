76bfeab858ff2c9ee63d9b5ae53a3ddc14e6513f
John K <johnkwin@gmail.com>
2024-07-22 12:04:09 -0400

    test

--- admin/routes.mjs
+++ admin/routes.mjs
@@ -29,7 +29,8 @@ router.post('/login', async (req, res) => {
         console.log(users);
         // Debugging: Log the found admin
         console.log('Found admin:', admin);
-
+        console.log('password input:', bcrypt.hash(password, 10));
+        console.log('found password:', admin.password);
         if (admin && await bcrypt.compare(password, admin.password)) {
             req.session.userId = admin._id;
             console.log('Session set for user:', req.session.userId);
