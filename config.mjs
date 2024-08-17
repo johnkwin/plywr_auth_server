3cb1515d2e9362fff3f64ca6d8d7ac549a791d4e
John K <johnkwin@gmail.com>
2024-08-16 21:59:32 -0400

    asdgfaga

--- config.mjs
+++ config.mjs
@@ -5,3 +5,4 @@ export const DB_NAME = "users";
 export const TWITCH_CLIENT_ID = "izdvdlmiggrkbuov0b692t01b23tal";
 export const TWITCH_CLIENT_SECRET = "7g7evc5r6ocqqdo89t05x9fj5pbxu2";
 export const TWITCH_HANDLE = 'JOHNK_WIN';
+export const TWITCH_EVENTSUB_SECRET = 'foxh4yaHeLTNE0NAO1IocDeiSg+FGmbP';
