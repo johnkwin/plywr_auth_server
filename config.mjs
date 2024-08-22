4b0cb2bb5f24283dd2fe5fc38fee83e549c118da
John K <johnkwin@gmail.com>
2024-08-21 22:53:18 -0400

    asfasfasfa

--- config.mjs
+++ config.mjs
@@ -3,6 +3,6 @@ export const DB_PASSWORD = "n9bT#tBAv35KwKb";
 export const DB_NAME = "users";
 
 export const TWITCH_CLIENT_ID = "cswpy9s97hrv5qtoslffl71tk7moxn";
-export const TWITCH_CLIENT_SECRET = "0llzrwkpni8cbyz5asm3ft8fdyxj5y";
+export const TWITCH_CLIENT_SECRET = "s6tyf3ky3y8vsdf0oxhoby18kkct5x";
 export const TWITCH_HANDLE = 'Player1USA';
 export const TWITCH_EVENTSUB_SECRET = 'foxh4yaHeLTNE0NAO1IocDeiSg+FGmbP';
