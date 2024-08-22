f72ba313265019f23236aba28d7d7bee1c4567fe
John K <johnkwin@gmail.com>
2024-08-21 23:45:20 -0400

    asdasdsadasa

--- user/routes.mjs
+++ user/routes.mjs
@@ -371,7 +371,8 @@ router.get('/oauth', async (req, res) => {
         const broadcasterId = await getBroadcasterId(access_token);
         user.broadcasterId = broadcasterId;
         const callbackUrl = 'https://join-playware.com/twitch/events';  // Replace with your callback URL
-        await ensureSubscriptions(getAppAccessToken(), broadcasterId, callbackUrl);
+        const AppAccessToken = await getAppAccessToken();
+        await ensureSubscriptions(AppAccessToken, broadcasterId, callbackUrl);
         await user.save();
 
         res.redirect('/user/dashboard');
